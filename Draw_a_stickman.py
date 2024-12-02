09a950bee364a312ddf94a956d1852c7c809821d
slothTaco <pickeringwilliamscott@gmail.com>
2024-12-02 11:56:13 -0600

    Create Draw_a_stickman.py

---
+++ Draw_a_stickman.py
@@ -0,0 +1,39 @@
+#Draw a person
+#Draw head
+#Move to position for torso
+#Draw torso
+#Move to position of arms
+#Draw arms
+#Move to position for legs
+#Draw legs
+
+
+#Draw a person
+from turtle import*
+#Draw head
+circle(20)
+#Move to position for torso
+right(90)
+#Draw torso
+forward(40)
+#Move to position of arms
+left(180)
+forward(20)
+right(90)
+#Draw arms
+forward(20)
+right(180)
+forward(40)
+right(180)
+forward(20)
+#Move to position for legs
+right(90)
+forward(20)
+#Draw legs
+left(35)
+forward(40)
+right(180)
+forward(40)
+left(115)
+forward(40)
+left(180)
